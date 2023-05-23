52eb6c50419559262c9b58bcae34972de5edf47c
Sean Siganoff <nationwideclassics@yahoo.com>
2023-05-23 14:30:00 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Daily Reminder
+      </header>
+    </div>
+  );
+}
+
+export default App;
